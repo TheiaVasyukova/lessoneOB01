bf1bcc5ec654c86156f1c95246327c6f1bc9ecfa
Theia <168746713+TheiaVasyukova@users.noreply.github.com>
2024-05-17 21:56:58 +0300

    add new second class

--- task.py
+++ task.py
@@ -10,3 +10,23 @@ class Task:
     def display_info(self):
         status = "Выполнено" if self.is_completed else "Не выполнено"
         return f"Задача: {self.description}, Срок выполнения: {self.due_date}, Статус: {status}"
+
+class TaskManager:
+    def __init__(self):
+        self.tasks = {}
+
+    def add_task(self, description, due_date):
+        if description not in self.tasks:
+            self.tasks[description] = Task(description, due_date)
+
+    def mark_task_completed(self, description):
+        if description in self.tasks:
+            self.tasks[description].mark_completed()
+
+    def get_pending_tasks(self):
+        return [task for task in self.tasks.values() if not task.is_completed]
+
+    def display_tasks(self):
+        for task in self.tasks.values():
+            print(task.display_info())
+
